7e2baf896fa17389fffe95f2d88e52f654caa554
jimmy0902 <jimmy.fans@live.cn>
2017-11-01 18:12:31 +0800

    修改为多层控制器

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'CONTROLLER_LEVEL'      =>  2,  //配置多层控制器
+	'DEFAULT_MODULE'        =>  'Home',  // 默认模块
+    'DEFAULT_CONTROLLER'    =>  'Index/Index', // 默认控制器名称
+    'DEFAULT_ACTION'        =>  'index', // 默认操作名称
 );
---
+++ Application/Home/Controller/Index/IndexController.class.php
@@ -0,0 +1,77 @@
+<?php
+namespace Home\Controller\Index;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        $this->display();
+    }
+
+
+    public function index1(){
+    	$this -> display();
+    }
+
+    public function about(){
+    	$this -> display();
+    }
+
+    public function blog(){
+    	$this -> display();
+    }
+
+    public function blog_alternate(){
+    	$this -> display();
+    }
+
+    public function blog_single(){
+    	$this -> display();
+    }
+
+    public function buttons_tables(){
+    	$this -> display();
+    }
+
+    public function contact(){
+    	$this -> display();
+    }
+
+    public function fullwidth_columns(){
+    	$this -> display();
+    }
+
+    public function images_dropcaps(){
+    	$this -> display();
+    }
+
+    public function list_blockquotes(){
+    	$this -> display();
+    }
+
+    public function portfolio_1col(){
+    	$this -> display();
+    }
+
+    public function portfolio_2col(){
+    	$this -> display();
+    }
+
+    public function portfolio_3col(){
+    	$this -> display();
+    }
+
+    public function portfolio_4col(){
+    	$this -> display();
+    }
+
+    public function portfolio_details(){
+    	$this -> display();
+    }
+
+    public function tabs_toggles(){
+    	$this -> display();
+    }
+
+    public function typography(){
+    	$this -> display();
+    }
+}
