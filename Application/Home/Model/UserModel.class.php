0ff9d02b58cab293a8e2421979758ff87abbce80
jimmy0902 <32455508+jimmy0902@users.noreply.github.com>
2017-11-05 21:52:23 +0800

    login register (#1)
    login: session cookie   register: token

--- Application/Home/Model/UserModel.class.php
+++ Application/Home/Model/UserModel.class.php
@@ -3,10 +3,23 @@ namespace Home\Model;
 use Think\Model;
 class UserModel extends Model {
 
-   // 3表示所有后， 2 表示更新的时候， 1 表示新增的时候
+
+   // 验证条件： 0 存在字段就验证，  1 必须验证， 2 值不为空的时候验证
+   //验证时间： 3表示所有后时候， 2 表示更新的时候， 1 表示新增的时候
+   
       protected $_auto = array(
            array('password'     , 'md5', 3, 'function'),   
            array('register_time', 'time', 1 , 'function'),  //新增的时候 插入时间
+      );
+
+
+     // 第一个数字为验证条件，第二个为验证时间
+      protected $_validate = array(
+ 
+           // array('username', '', 'username 存在', 0, 'unique', 1 ),  //有bug 通过 create后自动填充数据
+           array('username', 'require', '用户名不能为空'),
+           array('password', 'require', '密码不能为空'),
+           // array('password', 'confirmPassword', '两次密码输入不一致', 0, 'confirm'), //有bug
       );
 
 }
